bbca340621f4c0301019e1731a73e9ab18aed0c8
mal1kc <59576495+mal1kc@users.noreply.github.com>
2024-08-21 19:19:00 +0300

    fixed bugs added some features (multi skip)

---
+++ src/muscpy/utils.py
@@ -0,0 +1,132 @@
+import asyncio
+from typing import Generic, TypeVar
+
+import discord
+
+pool_K = TypeVar("pool_K")
+pool_V = TypeVar("pool_V")
+
+
+class SharedDict(Generic[pool_K, pool_V]):
+    def __init__(self):
+        self._lock = asyncio.Lock()
+        self._pool: dict[pool_K, pool_V] = {}
+
+    async def get(self, key: pool_K) -> pool_V | None:
+        async with self._lock:
+            return self._pool.get(key)
+
+    async def set(self, key: pool_K, value: pool_V):
+        async with self._lock:
+            self._pool[key] = value
+
+    async def delete(self, key: pool_K):
+        async with self._lock:
+            if key in self._pool:
+                del self._pool[key]
+
+
+class SharedList(Generic[pool_V]):
+    def __init__(self) -> None:
+        super().__init__()
+        self._lock = asyncio.Lock()
+        self._pool: list[pool_V] = []
+
+    def __getitem__(self, index: int) -> pool_V | None:
+        return asyncio.run(self.get(index))
+
+    def __setitem__(self, index: int, value: pool_V):
+        asyncio.run(self.set(index, value))
+
+    def __delitem__(self, value: pool_V):
+        asyncio.run(self.remove(value))
+
+    def __len__(self) -> int:
+        return len(self._pool)
+
+    def __iter__(self):
+        return iter(self._pool)
+
+    async def get(self, index: int) -> pool_V | None:
+        async with self._lock:
+            try:
+                return self._pool[index]
+            except IndexError:
+                return None
+
+    async def set(self, index: int, value: pool_V):
+        async with self._lock:
+            self._pool[index] = value
+
+    async def append(self, value: pool_V):
+        async with self._lock:
+            self._pool.append(value)
+
+    async def remove(self, value: pool_V):
+        async with self._lock:
+            self._pool.remove(value)
+
+    async def pop(self, index: int) -> pool_V | None:
+        async with self._lock:
+            try:
+                return self._pool.pop(index)
+            except IndexError:
+                return None
+
+    async def clear(self):
+        async with self._lock:
+            self._pool.clear()
+
+
+async def not_guild(interaction: discord.Interaction) -> bool:
+    if interaction.guild is None:
+        await interaction.response.send_message("Bot currently only supports guilds")
+        return True
+    return False
+
+
+async def get_voice_client(
+    interaction: discord.Interaction,
+    voice_ch: discord.VoiceChannel | None = None,
+    quiet: bool = True,
+) -> discord.VoiceClient | discord.VoiceProtocol | None:
+    if voice_ch is None:
+        if isinstance(interaction.user, discord.Member):
+            if isinstance(
+                interaction.user.voice, discord.VoiceState
+            ) and not isinstance(interaction.user.voice.channel, discord.StageChannel):
+                voice_ch = interaction.user.voice.channel
+
+    if voice_ch is None:
+        if not quiet:
+            await interaction.response.send_message(
+                "I can't find a voice channel to join"
+            )
+        return None
+
+    if await not_guild(interaction):
+        return None
+    # from now on we are sure that interaction.guild is not None
+    safe_guild: discord.Guild = interaction.guild  # pyright: ignore[reportAssignmentType]
+
+    voice_client = safe_guild.voice_client
+    if voice_client is not None and voice_client.channel != voice_ch:
+        try:
+            voice_client = await voice_ch.connect()
+        except discord.ClientException:
+            # already connected to a voice channel
+            # disconnect and reconnect to specified channel
+            await voice_client.disconnect(force=True)
+            voice_client = await voice_ch.connect()
+    elif voice_client is None:
+        voice_client = await voice_ch.connect()
+
+    if not quiet:
+        await interaction.response.send_message(f"Connected to {voice_ch.name}")
+    return voice_client
+
+
+def get_txt_channel(interaction: discord.Interaction) -> discord.TextChannel | None:
+    if interaction.channel:
+        if interaction.channel.type == discord.ChannelType.text:
+            return interaction.channel
--- ytdlp_test.py
+++ ytdlp_test.py
@@ -1,70 +1,95 @@
+# pyright: basic
 from pprint import pprint
+from typing import Any
 from yt_dlp import YoutubeDL
+
+
 common_ytdl_options = {
-        "format": "bestaudio/best",
-        "outtmpl": "%(extractor)s-%(id)s-%(title)s.%(ext)s",
-        "restrictfilenames": True,
-        "geo_bypass": True,
-        "nocheckcertificate": True,
-        "ignoreerrors": False,
-        "logtostderr": False,
-        "quiet": False,
-        "no_warnings": True,
-        "source_address": "0.0.0.0",
+    "format": "bestaudio/best",
+    "outtmpl": "%(extractor)s-%(id)s-%(title)s.%(ext)s",
+    "restrictfilenames": True,
+    "geo_bypass": True,
+    "nocheckcertificate": True,
+    "ignoreerrors": False,
+    "logtostderr": False,
+    "quiet": False,
+    "no_warnings": True,
+    "source_address": "0.0.0.0",
+    "default_search": "https://music.youtube.com/search?q=",
 }
-# ytdl_test_format_options = {
-#     # "format": "bestaudio/best",
-#     # "outtmpl": "%(extractor)s-%(id)s-%(title)s.%(ext)s",
-#     # "restrictfilenames": True,
-#     "noplaylist": False,
-#     "nocheckcertificate": True,
-#     "ignoreerrors": False,
-#     "logtostderr": False,
-#     "quiet": False, # probably should be True but I want to see errors
-#     # add plist item limit up to 100
-#     "playlist_items": "1-100",
-#     "no_warnings": True,
-#     "default_search": "auto",
-#     "source_address": "0.0.0.0",
-#     "extract_flat": True,
-#     "flat_playlist": True,
-# }
 
-# ytdlp_client = YoutubeDL(ytdl_test_format_options)
-
-# def ytdlp_test(url):
-#     with ytdlp_client:
-#         result = ytdlp_client.extract_info(url, download=False)
-#         return result
-    
-
-# def save_data_to_json(data, filename):
-#     import json
-#     with open(filename, 'w') as f:
-#         json.dump(data, f, indent=4)
-
-# if __name__ == "__main__":
-#     url = "https://music.youtube.com/watch?v=2DOVyJGK5PA&list=RDAMVM8L__O1hElCE"
-#     data = ytdlp_test(url)
-#     save_data_to_json(data, "data.json")
+ytdl_search_options = {
+    "noplaylist": True,
+    "playlist_items": "1-5",
+    "default_search": "https://music.youtube.com/search?q=",
+    "extract_flat": True,
+    "flat_playlist": True,
+    **common_ytdl_options,
+}
 
 
-def search_test():
-    
-    ytdl_search_options = {
-        "noplaylist": True,
-        "playlist_items": "1-5",
-        "default_search": "https://music.youtube.com/search?q=",
-        "extract_flat": True,
-        "flat_playlist": True,
-        **common_ytdl_options
-    }
+ytdl_glbl_format_options = {
+    "noplaylist": False,
+    "playlist_items": "1-100",
+    "default_search": "auto",
+    "extract_flat": True,
+    "flat_playlist": True,
+    **common_ytdl_options,
+}
 
+
+ytldl_single_url_options = {"noplaylist": True, **common_ytdl_options}
+
+
+def save_data_to_json(data: Any, filename: str):
+    import json
+
+    with open(filename, "w") as f:
+        json.dump(data, f, indent=4)
+
+
+def search_extract():
     with YoutubeDL(ytdl_search_options) as ydl:
         result = ydl.extract_info("ytsearch5:the boys finale ost", download=False)
         return result
-    
+
+
+def extract_info_glbl_options(url: str) -> dict[str, Any] | None:
+    with YoutubeDL(ytdl_glbl_format_options) as ydl:
+        result = ydl.extract_info(url, download=False)
+        return result
+
+
+# track data with playlist
+def pl_extract():
+    url = "https://music.youtube.com/playlist?list=OLAK5uy_noi_byHFvSNxfA3GziKw2xKIIc9_EYtoE"
+    return extract_info_glbl_options(url)
+
+
+def pl_item_extract():
+    url = "https://music.youtube.com/watch?v=g7RGN7MJR1s&list=OLAK5uy_noi_byHFvSNxfA3GziKw2xKIIc9_EYtoE"
+    return extract_info_glbl_options(url)
+
+
+def item_extract():
+    url = "https://www.youtube.com/watch?v=5jKkLjh6K0Y"
+    return extract_info_glbl_options(url)
+
 
 if __name__ == "__main__":
-    pprint(search_test())
+    test_data_result = pl_extract()
+    save_data_to_json(test_data_result, "./temp/pl_result.json")
+    pprint(test_data_result)
 
+    test_data_result = search_extract()
+
+    save_data_to_json(test_data_result, "./temp/search_result.json")
+    pprint(test_data_result)
+
+    test_data_result = pl_item_extract()
+    save_data_to_json(test_data_result, "./temp/pl_item_result.json")
+    pprint(test_data_result)
+
+    test_data_result = item_extract()
+    save_data_to_json(test_data_result, "./temp/item_result.json")
+    pprint(test_data_result)
